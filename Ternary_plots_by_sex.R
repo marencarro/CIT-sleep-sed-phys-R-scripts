d1be8d5a0fff0071a1de3631949918f69baa2c60
Maren C Frogner Werner <74731502+marencarro@users.noreply.github.com>
2025-09-29 08:56:15 +0200

    Update Ternary_plots_by_sex.R

--- Ternary_plots_by_sex.R
+++ Ternary_plots_by_sex.R
@@ -8,21 +8,23 @@ citation("ggtern")
 
 
 #Get datafile
-TS <- read.spss("YH4AllCohorts_clean_use_paper_1.sav", to.data.frame=TRUE)
-Ternary_sample <- TS[TS$Sample_paper1_mainanalysis_SleepSedLIPAMVPA=="Included"&is.na(TS$CIT_Sev_index_010)==0,]
+TS <- read.spss("df.sav", to.data.frame=TRUE)
+#Use filter, if applicable, & remove missing values from exposure variable
+Ternary_sample <- TS[TS$Filter=="Included"&is.na(TS$ex_var)==0,] 
 
-#Convert from seconds to minutes
-Ternary_sample$hvileMean3_min <- Ternary_sample$hvileMean3/60
-Ternary_sample$hardMean3_min <- Ternary_sample$hardMean3/60
-Ternary_sample$lettMean3_min <- Ternary_sample$lettMean3/60
+#Convert from seconds to minutes, if applicable
+Ternary_sample$SB_min <- Ternary_sample$SB/60
+Ternary_sample$MVPA_min <- Ternary_sample$MVPA/60
+Ternary_sample$LIPA_min <- Ternary_sample$LIPA/60
+Ternary_sample$sleep_min <- Ternary_sample$sleep/60
 
 #Create ternary sample
 Ternary_sample <- Ternary_sample %>%
-  mutate(total = sleepMean3_min+hvileMean3_min+lettMean3_min+hardMean3_min,
-         sleep = sleepMean3_min/total,
-         sedentary = hvileMean3_min/total,
-         LIPA = lettMean3_min/total,
-         MVPA = hardMean3_min/total)
+  mutate(total = sleep_min+SB_min+LIPA_min+MVPA_min,
+         sleep = sleep_min/total,
+         sedentary = SB_min/total,
+         LIPA = LIPA_min/total,
+         MVPA = MVPA_min/total)
 
 #Defining levels Female and male
 is.factor(Ternary_sample$Sex)
@@ -31,7 +33,7 @@ levels(Ternary_sample$Sex) <- c("Female", "Male")
 
 #Arranging direction of CIT score - going from 0 to 10
 Ternary_sample <- Ternary_sample %>%
-  arrange(CIT_Sev_index_010)
+  arrange(ex_var)
 
 # Test with Sleep, sedentary MVPA
 ggtern(data = Ternary_sample, aes(x = sleep, y = sedentary, z = MVPA))+
@@ -44,12 +46,12 @@ ggtern(data = Ternary_sample, aes(x = sleep, y = sedentary, z = MVPA))+
 #With colours
 library(viridis)
 
-#Sleep Sedentary MVPA
+#Sleep Sedentary (SB) MVPA
 ggtern(data = Ternary_sample, aes(x = sleep, y = sedentary, z = MVPA, color = CIT_Sev_index_010))+
   geom_point (size = 1)+
   facet_wrap(~Sex)+
-  labs(title = "Ternary plot Sleep-Sed-MVPA",
-       x = "Sleep", y = "Sed", z = "MVPA", color = "CIT Sev score (0-10)")+
+  labs(title = "Ternary plot Sleep-SB-MVPA",
+       x = "Sleep", y = "SB", z = "MVPA", color = "CIT Sev score (0-10)")+
   scale_color_viridis_c(option = "D", direction = -1)+
   theme_bw()
 
@@ -58,8 +60,8 @@ ggtern(data = Ternary_sample, aes(x = sleep, y = sedentary, z = MVPA, color = CI
 ggtern(data = Ternary_sample, aes(x = sleep, y = sedentary, z = LIPA, color = CIT_Sev_index_010))+
   geom_point (size = 1)+
   facet_wrap(~Sex)+
-  labs(title = "Ternary plot Sleep-Sed-LIPA",
-       x = "Sleep", y = "Sed", z = "LIPA", color = "CIT Sev score (0-10)")+
+  labs(title = "Ternary plot Sleep-SB-LIPA",
+       x = "Sleep", y = "SB", z = "LIPA", color = "CIT Sev score (0-10)")+
   scale_color_viridis_c(option = "D", direction = -1)+
   theme_bw()
 
@@ -76,8 +78,9 @@ ggtern(data = Ternary_sample, aes(x = sleep, y = LIPA, z = MVPA, color = CIT_Sev
 ggtern(data = Ternary_sample, aes(x = sedentary, y = LIPA, z = MVPA, color = CIT_Sev_index_010))+
   geom_point (size = 1)+
   facet_wrap(~Sex)+
-  labs(title = "Ternary plot Sed-LIPA-MVPA",
-       x = "Sed", y = "LIPA", z = "MVPA", color = "CIT Sev score (0-10)")+
+  labs(title = "Ternary plot SB-LIPA-MVPA",
+       x = "SB", y = "LIPA", z = "MVPA", color = "CIT Sev score (0-10)")+
   scale_color_viridis_c(option = "D", direction = -1)+
   theme_bw()
+
 
